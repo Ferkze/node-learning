ed1b02a0eb49cf01eb256c96ce57ab95db676c06
Ferkze <ferkemartins@gmail.com>
2020-07-17 07:09:11 -0300

    Adicionado arquivo de leitura e parse de JSON

---
+++ fs/lendo-jsons.js
@@ -0,0 +1,25 @@
+const fs = require('fs')
+
+fs.readFile('teste.json', function(err, data) {
+
+	if (err) {
+		console.log('deu ruim:', err)
+		return
+	}
+
+	// JSON é um objeto padrão do Node
+	// Assim como no navegador, document, window são objetos padrões para o JavaScript
+	// JSON.parse(data)
+	// ele é usado para converter texto (string) em objeto (json)
+
+	// o log acima vai mostrar toda a array de objetos que tinha dentro do arquivo
+
+	// você pode guardar o objeto json em uma variavel
+
+	var dados = JSON.parse(data)
+
+	for (var i = 0; i < dados.length; i++) {
+		console.log('Esses são os dados da lista:', dados[i])
+	}
+
+})
